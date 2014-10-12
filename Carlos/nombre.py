24e8b9bbb2f66526645936a80e111b206dfc8e0e
Carlos Manrique <Manri98@users.noreply.github.com>
2014-10-13 00:39:00 +0200

    Eliminada variable innecesaria

--- Carlos/nombre.py
+++ Carlos/nombre.py
@@ -1,9 +1,8 @@
 import time
 nombre = input("Introduzca su nombre: ")
 time.sleep(1)
-largo = len(nombre)
 contador = 0
-for contador in range(1, largo):
+for contador in range(1, len(nombre)):
     print(nombre[:contador])
-for contador in range(largo, 0, -1):
+for contador in range(len(nombre), 0, -1):
     print(nombre[:contador])
