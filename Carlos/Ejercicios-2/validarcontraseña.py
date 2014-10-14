d35f64e28416a77beab6bab1261edad6935e3480
Carlos Manrique Enguita <carlos@debian>
2014-10-14 23:51:02 +0200

    Añadidos los criterios especificos
    Ahora la función reconoce cada caso particular de validación e imprime el error en la pantalla.

--- Carlos/Ejercicios-2/validarcontraseña.py
+++ Carlos/Ejercicios-2/validarcontraseña.py
@@ -1,11 +1,23 @@
 def validarcontraseña():
     contraseña = input("Introduzca una contraseña: ")
     if chr(32) in contraseña:
-        print("La contraseña elegida no es segura.\nDebe tener un mínimo de 8 caracteres.\nDebe incluir al menos una mayúscula, minúscula, número y carácter alfanumérico. ")
+        print("No puede haber espacios en la contraseña. ")
+        contraseña = input("Introduzca una nueva contraseña: ")    
+    if len(contraseña)<8:
+        print("La contraseña debe contener un mínimo de 8 caracteres. ")
         contraseña = input("Introduzca una nueva contraseña: ")
-
-    while len(contraseña)<8 or contraseña.isalnum()==True or contraseña.islower()==True or contraseña.isupper()==True or contraseña.isdigit()==True:
-        print("La contraseña elegida no es segura.\nDebe tener un mínimo de 8 caracteres.\nDebe incluir al menos una mayúscula, minúscula, número y carácter alfanumérico. ")
+    if contraseña.isalnum()==True:
+        print("La contraseña debe contener al menos un caracter no alfanumérico. ")
+        contraseña = input("Introduzca una nueva contraseña: ")
+    if contraseña.islower()==True:
+        print("La contraseña debe contener al menos una mayúscula. ")
+        contraseña = input("Introduzca una nueva contraseña: ")
+    if contraseña.isupper()==True:
+        print("La contraseña debe contener al menos una minúscula. ")
+        contraseña = input("Introduzca una nueva contraseña: ")
+    if contraseña.isdigit()==True:
+        print("La contraseña debe contener letras. ")
         contraseña = input("Introduzca una nueva contraseña: ")
     print("La contraseña es segura")
     return True
+validarcontraseña()
