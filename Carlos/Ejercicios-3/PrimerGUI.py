c92962e791d6eff10ee0ed40e6856a9694959cc4
Carlos Manrique Enguita <Manri98@users.noreply.github.com>
2014-10-21 18:27:08 +0200

    Añadido primer ejercicio de GUI
    Incompleto

---
+++ Carlos/Ejercicios-3/PrimerGUI.py
@@ -0,0 +1,53 @@
+def multiply():
+    w.insert(0,"*")
+    w.pack()
+
+def divide():
+    w.insert(0,"/")
+    w.pack()
+
+def result():
+    k = 0
+    variable = svalue.get()
+    for i in range (0,len(variable)):
+        if not variable[k].isdigit():
+            print("no es un numero")
+            
+        else:
+            print("1")
+        k = k+1
+    
+
+from tkinter import *
+
+
+window = Tk(className="Primer GUI")
+
+foo = Label(window,text="Calculadora en Python")
+foo.pack()
+
+svalue = StringVar()
+w = Entry(window,textvariable=svalue)
+w.pack()
+
+foo = Button(window,text="Enter",command= button01_function)
+foo.pack()
+
+foo = Button(window,text="+",command= addition)
+foo.pack(padx=5,side=LEFT)
+
+foo = Button(window,text="-",command= sustract)
+foo.pack(padx=5,side=LEFT)
+
+foo = Button(window,text="*",command= multiply)
+foo.pack(padx=5,side=LEFT)
+
+foo = Button(window,text="/",command= divide)
+foo.pack(padx=5,side=LEFT)
+
+foo = Button(window,text="=",command= result)
+foo.pack(padx=5,side=LEFT)
+
+window.mainloop()
+
+#     len(svalue)
