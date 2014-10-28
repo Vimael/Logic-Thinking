76c6b17a3f54ac8d2f95d524a2b37847e210845e
josecurioso <jotajotanora@gmail.com>
2014-10-28 20:52:14 +0100

    Optimizadas las recomendaciones
    El programa ya funciona aunque vas a acabar harto de la lista de
    Schindler

--- Jose/Ejercicios-4/Recomendaciones/Recomendaciones.py
+++ Jose/Ejercicios-4/Recomendaciones/Recomendaciones.py
@@ -1,19 +1,12 @@
 from tkinter import *
-r1 = 1
-r2 = 1
-r3 = 1
-r4 = 1
-r5 = 1
-r6 = 1
-r7 = 1
 def verificar():
-        global r1
-        global r2
-        global r3
-        global r4
-        global r5
-        global r6
-        global r7
+        r1 = 0
+        r2 = 0
+        r3 = 0
+        r4 = 0
+        r5 = 0
+        r6 = 0
+        r7 = 0
         import webbrowser
         if v1.get():
                 r1 = 20
@@ -31,17 +24,17 @@ def verificar():
                 r7 = -10
         res = r1+r2+r3+r4+r5+r6+r7
         if res >= 50:
-                webbrowser.open("")
+                webbrowser.open("http://www.youtube.com/watch?v=QH2-TGUlwu4")
         if res >= 20 and res < 50:
-                webbrowser.open("")
+                webbrowser.open("http://www.youtube.com/watch?v=odf1sSPdZ1c")
         if res >= 0 and res < 20:
-                webbrowser.open("")
+                webbrowser.open("http://www.youtube.com/watch?v=rJNInqZG2XI")
         if res < 0 and res >= -20:
-                webbrowser.open("")
+                webbrowser.open(" http://www.youtube.com/watch?v=6zapgcgtdzw")
         if res < -20 and res >= -40:
-                webbrowser.open("")
+                webbrowser.open("http://www.youtube.com/kffacxfA7G4?t=44s")
         if res < -40 and res >= -60:
-                webbrowser.open("")
+                webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
         print(res)
   
 
@@ -63,13 +56,13 @@ ci5 = PhotoImage(file="muriendose.png")
 ci6 = PhotoImage(file="sorprendido.png")
 ci7 = PhotoImage(file="triste.png")
 
-ch1 = Checkbutton(ventana, image=ci1, text="Me siento eufórico", var=v1)
-ch2 = Checkbutton(ventana, image=ci2, text="Me siento triste", var=v2)
-ch3 = Checkbutton(ventana, image=ci3, text="Me siento cansado", var=v3)
-ch4 = Checkbutton(ventana, image=ci4, text="Me siento eufórico", var=v4)
-ch5 = Checkbutton(ventana, image=ci5, text="Me siento triste", var=v5)
-ch6 = Checkbutton(ventana, image=ci6, text="Me siento cansado", var=v6)
-ch7 = Checkbutton(ventana, image=ci7, text="Me siento cansado", var=v7)
+ch1 = Checkbutton(ventana, image=ci1, var=v1)
+ch2 = Checkbutton(ventana, image=ci2, var=v2)
+ch3 = Checkbutton(ventana, image=ci3, var=v3)
+ch4 = Checkbutton(ventana, image=ci4, var=v4)
+ch5 = Checkbutton(ventana, image=ci5, var=v5)
+ch6 = Checkbutton(ventana, image=ci6, var=v6)
+ch7 = Checkbutton(ventana, image=ci7, var=v7)
 cm = Button(ventana, image=bg, text="Verificar", command=verificar, width=32, height=32)
 
 ch1.grid(row=0, column=0)
